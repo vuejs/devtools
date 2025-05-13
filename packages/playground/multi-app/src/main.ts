d6e3ff683a11370d12fbd286b68c1ed1d67fbeba
Alex <alexzhang1030@foxmail.com>
2025-05-13 13:40:58 +0800

    chore: add playground

--- packages/playground/multi-app/src/main.ts
+++ packages/playground/multi-app/src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 
 import App2 from './App2.vue'
 import App from './App.vue'
+import Iframe from './components/Iframe/index.vue'
 
 import './style.css'
 import 'uno.css'
@@ -10,6 +11,10 @@ const app = createApp(App)
 
 const app2 = createApp(App2)
 
+const app3 = createApp(Iframe)
+
 app.mount('#app')
 
 app2.mount('#app2')
+
+app3.mount('#app3')
