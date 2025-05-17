3c015a3c57eb4469f1a06ecf8eae2812241407cc
arlo <webfansplz@gmail.com>
2025-05-17 16:50:55 +0800

    chore: update playground

--- packages/playground/reactivity-tracing/src/App.vue
+++ packages/playground/reactivity-tracing/src/App.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import Number from './components/Number.vue'
 
-const count = ref(0)
+const num = ref(1)
+const count = ref(num.value)
+
+const doubleCount = computed(() => count.value * 2)
+
+const fourCount = computed(() => count.value * 4)
+
+console.log('x', count)
 </script>
 
 <template>
   <div class="m-auto mt-3 h-30 w-60 flex flex-col items-center justify-center rounded bg-[#363636]">
     App
     <div>Count: <Number :num="count" /></div>
+    <div>Double Count: <Number :num="doubleCount" /></div>
+    <div>Four Count: <Number :num="fourCount" /></div>
     <button @click="count++">
       ++
     </button>
