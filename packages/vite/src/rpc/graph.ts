69f71ba9bdf109fd15f40951ada0be6ec7337d4c
arlo <webfansplz@gmail.com>
2025-07-24 21:27:57 +0800

    fix: filter graph modules

--- packages/vite/src/rpc/graph.ts
+++ packages/vite/src/rpc/graph.ts
@@ -24,7 +24,30 @@ export function getGraphFunctions(ctx: RpcFunctionCtx) {
             })
           : null
       ) || []
-      return modules
+      const filteredModules = modules.filter((m) => {
+        return m.id.match(/\.(vue|js|ts|jsx|tsx|html|json)($|\?v=)/)
+      })
+      const graph = filteredModules.map((i) => {
+        function searchForVueDeps(id: string, seen = new Set<string>()): string[] {
+          if (seen.has(id))
+            return []
+          seen.add(id)
+          const module = modules.find(m => m.id === id)
+          if (!module)
+            return []
+          return module.deps.flatMap((i) => {
+            if (filteredModules.find(m => m.id === i))
+              return [i]
+            return searchForVueDeps(i, seen)
+          })
+        }
+
+        return {
+          id: i.id,
+          deps: searchForVueDeps(i.id),
+        }
+      })
+      return graph
     },
   }
 }
