ace0d498bde841e7f16d80157cdaf7424afe71fb
James Garbutt <43081j@users.noreply.github.com>
2025-09-14 22:15:21 +0100

    perf: drop `execa`
    We don't actually use it in the main vite package, so that one is an
    easy win.
    
    In the electron package, we use it only to launch a well known path,
    i.e. we don't use any features of `execa`. So we should be able to
    switch to using the built in `child_process` module directly.

--- packages/electron/src/cli.ts
+++ packages/electron/src/cli.ts
@@ -1,13 +1,13 @@
+import { spawnSync } from 'node:child_process'
 import electron from 'electron'
-import { execaSync } from 'execa'
 import { resolve } from 'pathe'
 
 const appPath = decodeURIComponent(resolve(new URL('../dist/app.cjs', import.meta.url).pathname))
 const argv = process.argv.slice(2)
 
-const result = execaSync(electron as unknown as string, [appPath].concat(argv), {
+const result = spawnSync(electron as unknown as string, [appPath].concat(argv), {
   stdio: 'inherit',
-  windowsHide: false,
+  shell: process.platform === 'win32',
 })
 
-process.exit(result.exitCode)
+process.exit(result.status ?? 0)
