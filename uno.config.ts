c7402989a867aa150c3ea159250e8e73e17b6755
arlo <webfansplz@gmail.com>
2026-06-25 20:53:56 +0800

    chore: update

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'unocss'
 
-import config from './packages/client/uno.config'
+// @ts-expect-error config files are loaded directly by tsx in lint/UnoCSS.
+import config from './packages/client/uno.config.ts'
 
 export default defineConfig({
   ...config,
